b379de39d0e495a60219abfd5e53e859048a5b75
monsterJohan <nikfil97@gmail.com>
2022-09-03 13:17:43 +0200

    Implemented Task5

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,26 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int i = 1; i <= height; i++) {
+            if (i <= height / 2) {
+                for (int j = 1; j <= height; j++) {
+                    if (j <= i - 1 || j > height - i + 1) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("8");
+                    }
+                }
+                System.out.println();
+            } else {
+                for (int j = 1; j <= height; j++) {
+                    if (j <= height - i || j > i) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("8");
+                    }
+                }
+                System.out.println();
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,22 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-
+        for (int i = 1; i <= sideLength; i++) {
+            if (i != 1 && i != sideLength) {
+                for (int j = 1; j <= sideLength; j++) {
+                    if (j != 1 && j != sideLength) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("8");
+                    }
+                }
+                System.out.println();
+            } else {
+                for (int j = 1; j <= sideLength; j++) {
+                    System.out.print("8");
+                }
+                System.out.println();
+            }
+        }
     }
 }
